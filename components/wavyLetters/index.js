9bdbfc84e499cab3e7121b89601711e19c75059c
Nguyen Cong Dat <nguyencongdat@192.168.1.6>
2022-10-05 15:42:32 +0700

    done phase 1

--- components/wavyLetters/index.js
+++ components/wavyLetters/index.js
@@ -1,32 +1,63 @@
 import styles from "./wavyLetters.module.css";
 
 export default function WavyLetters(props) {
-  const { word, timeDuration, fontWeight, fontFamily, fontSize ,marginLeft} = props;
+  const {
+    word,
+    timeDuration,
+    fontWeight,
+    fontFamily,
+    fontSize,
+    styleWord,
+    linear,
+  } = props;
 
   const styleProps = {
-    fontWeight: fontWeight || 'normal',
+    fontWeight: fontWeight || "normal",
     fontFamily: fontFamily || "Lausanne-Regular,sans-serif",
     fontSize: fontSize || "52px",
-    // marginLeft: marginLeft || "1px"
   };
 
-  let time = timeDuration || 0;
+  const calculatorTimeDuration = (index) => {
+    let time = timeDuration || 0;
+    return `${0.8 + time + (index + 1) / 10}`;
+  };
+
+  const calculatorLinear = (index) => {
+    const totalLetter = word.split("").length;
+    const { from, to } = linear;
+    const red = from.r + ((index + 1) * (to.r - from.r)) / totalLetter;
+    const green = from.g + ((index + 1) * (to.g - from.g)) / totalLetter;
+    const blue = from.b + ((index + 1) * (to.b - from.b)) / totalLetter;
+    const alpha = from.a + ((index + 1) * (to.a - from.a)) / totalLetter;
+    
+    return `rgba(${red},${green},${blue},${alpha})`;
+  };
 
   const renderWord = word.split("").map((letter, index) => {
-    index % 10 === 0 && time++;
-    letter === " " && (letter = <span style={{ marginLeft: "13px", display:"inline-block" }}></span>);
+    letter === " " &&
+      (letter = (
+        <span style={{ marginLeft: "13px", display: "inline-block" }}></span>
+      ));
 
-    const timeDuration = `${time}.${index}`;
+    const timeDuration = calculatorTimeDuration(index);
     return (
       <div
         key={index}
         className={styles.letter}
-        style={{ animationDuration: `${timeDuration}s`, ...styleProps }}
+        style={{
+          animationDuration: `${timeDuration}s`,
+          color: `${linear ? calculatorLinear(index) : ""}`,
+          ...styleProps,
+        }}
       >
         {letter}
       </div>
     );
   });
 
-  return <div className={styles.wavy_letters}>{renderWord}</div>;
+  return (
+    <div style={{ ...(styleWord || {}) }} className={styles.wavy_letters}>
+      {renderWord}
+    </div>
+  );
 }
